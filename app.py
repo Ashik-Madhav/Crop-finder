1b94b1fea669b31b6308bd6f12581ebb77be56de
Ashik-Madhav <ashik47229@gmail.com>
2025-08-06 16:29:06 +0400

    Update app.py

--- app.py
+++ app.py
@@ -76,5 +76,4 @@ def predict():
         return render_template('result.html', prediction=f"Error: {e}", info="")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
